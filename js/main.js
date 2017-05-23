81696ca74b4b10a12ed6105d61d082869134a47f
stevblon <stevblon@student.arteveldehs.be>
2017-05-23 15:43:28 +0200

    mobile menu

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function myFunction() {
+
+    var x = document.getElementById("unfold");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+var topnav = document.querySelector
+}
